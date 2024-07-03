817a165c05379ae8b0ddea3423b605476c091a4d
drashtipatell18 <165013408+drashtipatell18@users.noreply.github.com>
2024-07-03 16:20:22 +0530

    Logout Issue Solved

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -81,6 +81,7 @@ class HomeController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
+        $request->session_destroy();
         return redirect('/login');
     }
 }
